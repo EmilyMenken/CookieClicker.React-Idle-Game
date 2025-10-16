7f2e708050bfc01a42b63f24987fb851df52fa5c
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-16 13:23:48 -0700

    added app.jsx and a pumpkin component

---
+++ src/components/Pumpkin.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Header from "../components/Header"
+import Pumpkin from "../components/Pumpkin"
+import Footer from "../components/Footer"
+
+function App()
+{
+    return(
+        <div>
+            <Header />
+            <Pumpkin />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
