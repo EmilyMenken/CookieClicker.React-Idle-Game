7405c101927e0781b98de11bfa92306ae5cc8b6d
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-16 13:36:39 -0700

    added core state reqs

--- src/components/Pumpkin.jsx
+++ src/components/Pumpkin.jsx
@@ -2,6 +2,25 @@ import { useState } from "react";
 
 function Game()
 {
+    const [clickCount, setCount] = useState(0);
+    const [rotten, setEndGame] = useState(flase);
+    const [pumpkinType, setType] = ([{
+        size: "small",
+        color: "yellow",
+        count: 0
+    },
+    {
+        size: "medium",
+        color: "peach",
+        count: 0
+    },
+    {
+        size: "large",
+        color: "orange",
+        count: 0
+    }]);
+    
+    
     return(
         <div>
         
@@ -10,3 +29,10 @@ function Game()
 }
 
 export default Game
+
+// p1
+//  small, yellow
+// p2
+//  medium, peach
+// p3
+// large, orange
