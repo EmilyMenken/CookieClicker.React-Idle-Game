f2e50767b6378c5961b8213c530d3fff3d9572bd
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-17 10:13:05 -0700

    reworking game function to make it more functinal as a count down

--- src/components/Pumpkin.jsx
+++ src/components/Pumpkin.jsx
@@ -16,28 +16,33 @@ function Game()
     const [pumpkinType, setType] = useState([{
         size: "small",
         color: "yellow",
+        img: pumpkinYellow,
         count: 0
     },
     {
         size: "medium",
         color: "peach",
+        img: pumpkinPeach,
         count: 0
     },
     {
         size: "large",
         color: "orange",
+        img: pumpkinOrange,
         count: 0
     },
     {
         size: "rotten",
         color: "brown",
+        img: pumpkinRotten,
         count: 0
     }]);
-
     const [timeLeft, setTimeLeft] = useState(30); // timer
     const [timerRunning, setTimerRunning] = useState(false);
-    
 
+    clickCount = 90;
+    
+//Timer functions
     function startTimer() {
         if (timerRunning) return; // sets single timer
         setTimerRunning(true);
@@ -68,47 +73,60 @@ function Game()
     function handleClick(){
         console.log("CLICKED"); 
     }
-    
+
+//get random image function
     function randImg()
     {
         const rand = Math.floor(Math.random() * 4); // 0 - 3
         if(rand === 0)
         {
-            return pumpkinYellow
+            return pumpkinType[0]
         }
         else if(rand === 1)
         {
-            return pumpkinPeach
+            return pumpkinType[1]
         }
         else if(rand === 3)
         {
-            return pumpkinOrange
+            return pumpkinType[2]
         }
         else //rand === 4
         {
-            return pumpkinRotten
+            return pumpkinType[3]
         }
     }
 
-
-    //put return in while loop
-    //  while time != 0 and pumpkinTypes[3].count === 0
-    let flag = true;
+    const flag = true;
     if(flag)
     do{
         return(
         <div className="main">
-            <h1> {timeLeft} seconds </h1>
+            <h2>Bring the Click Count to 0 before time runs out!</h2>
+            <h3> {timeLeft} seconds </h3>
             <button className= "timerButton" onClick={startTimer}>Start Timer</button>
             <button className= "timerButton" onClick={stopTimer}>Stop Timer</button>
             <button className= "timerButton" onClick={resetTimer}>Reset Timer</button>
             
             {/* <button className= "pumpkinButton" onClick={handleClick}><img src={pumpkinGroup} alt="icon"/></button> */}
             <p>Current Clicks: {clickCount}</p>
-            <button className= "pumpkinButton" onClick={() => setCount(clickCount + 1)}><img src={randImg()} alt="pumpkin"/></button>
+            <button className= "pumpkinButton" onClick={() => setCount(clickCount - 1)}><img src={randImg()} alt="pumpkin"/></button>
         </div>
         )
-    }while(timer < 0 && pumpkinType[3].count === 0) 
+    }while(timer < 0 && clickCount > 0) 
+    return(
+        <div className="game-over">
+            <h2>Game Over!</h2>
+            <h4>Stats:</h4>
+            <p>You had {clickCount} clicks left</p>
+            <p>You clicked a total {pumpkinType[0].count + pumpkinType[1].count + pumpkinType[2].count + pumpkinType[3].count} times</p>
+            <ul>
+                <li id="yellow">Yellow Pumpkin clicks: +{pumpkinType[0].count}</li>
+                <li id="peach">Peach Pumpkin clicks: +{pumpkinType[1].count}</li>
+                <li id="orange">Orange Pumpkin clicks: +{pumpkinType[2].count}</li>
+                <li id="rotten">Rotten Pumpkin clicks: -{pumpkinType[3].count}</li>
+            </ul>
+        </div>
+    )
 }
 
 
