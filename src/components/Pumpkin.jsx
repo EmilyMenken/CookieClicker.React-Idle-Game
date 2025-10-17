d37c3dcca9051c7d9206dced2f7e5327087a67a5
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-17 13:51:35 -0700

    fixed some issues with ending games nad rendering images

--- src/components/Pumpkin.jsx
+++ src/components/Pumpkin.jsx
@@ -3,44 +3,27 @@ import { useState } from "react";
 import pumpkinYellow from "../assets/pumpkinA.png";
 import pumpkinPeach from "../assets/pumpkinB.png";
 import pumpkinOrange from "../assets/pumpkinC.png";
-import pumpkinGroup from "../assets/pumpkinGroup.png";
 import pumpkinRotten from "../assets/pumpkinRotten.png";
+import "../styles.css";
 
 let timer;
 
 
 function Game()
 {
-    const [clickCount, setCount] = useState(0);
+    const [clickCount, setCount] = useState(90);
+    const [totalClicks, setTotalClicks] = useState(0);
     const [gameOver, setGameOver] = useState(false);
-    const [pumpkinType, setType] = useState([{
-        size: "small",
-        color: "Yellow",
-        img: pumpkinYellow,
-        count: 0
-    },
-    {
-        size: "medium",
-        color: "Peach",
-        img: pumpkinPeach,
-        count: 0
-    },
-    {
-        size: "large",
-        color: "Orange",
-        img: pumpkinOrange,
-        count: 0
-    },
-    {
-        size: "rotten",
-        color: "Brown",
-        img: pumpkinRotten,
-        count: 0
-    }]);
+    const [pumpkinType, setType] = useState([
+    { id: 1, size: "small", color: "Yellow", img: pumpkinYellow, count: 0 },
+    { id: 2, size: "medium", color: "Peach", img: pumpkinPeach, count: 0 },
+    { id: 3, size: "large", color: "Orange", img: pumpkinOrange, count: 0 },
+    { id: 4, size: "rotten", color: "Brown", img: pumpkinRotten, count: 0 },
+    ]);
     const [timeLeft, setTimeLeft] = useState(30); // timer
     const [timerRunning, setTimerRunning] = useState(false);
+    const [randomPumpkin, setRandomPumpkin] = useState(getRandomPumpkin());
 
-    clickCount = 90;
     
 //Timer functions
     function startTimer() {
@@ -71,81 +54,104 @@ function Game()
     }
 
 //get random image function
-    function randImg()
+    function getRandomPumpkin()
     {
         if(!gameOver)
         {
             const rand = Math.floor(Math.random() * pumpkinType.length); // 0 - 3
-            return pumpkinType[rand];
+            return rand;
         }
+        return 0;
     }
-
-    const randomImg = randImg().img
-
+    
+    const pumpkin = pumpkinType[randomPumpkin];
 
     function handleClick() {
-        if(gameOver)
-            return;
-        
-        if(!randomImg)
-            return;
-        
-        if(randomImg.img = pumpkinRotten)
-        {
+    if (gameOver) 
+        return;
+
+    setTotalClicks(function (t) {
+      return t + 1;
+    });
+
+    setCount(function (c) { // decrease clicks and check for 0
+        c = c -1;
+        if (c <= 0) {
             setGameOver(true);
-            setCount(c => c + 1);
-            return;
         }
+        return c;
+    });
 
-        setCount(c => c + 1);
+    if (pumpkin.color === "Brown") //end game if clicked
+    {
+      setGameOver(true);
+      return;
+    }
 
-        setType(function(prevImages) {
-            const updated = prevImages.map(function(pImg){
-                if(pImg.img === randomImg)
-                {
-                    return{...pImg, count: pImg.count + 1};
-                }
-                else
-                {
-                    return pImg;
-                }
-            });
-            return updated
-        })}
+    setType(function (prev) { //count clicks for specific type
+      const updated = prev.map(function (p) {
+        if (p.id === pumpkin.id) 
+        {
+          return { ...p, count: p.count + 1 };
+        } 
+        else 
+        {
+          return p;
+        }
+      });
+      return updated;
+    });
+    
+    setRandomPumpkin(getRandomPumpkin()); //get new rand pumpkin after every click NOT second
+  }
 
-    const flag = true;
-    if(flag)
-    do{
+    if(gameOver)
+    {
         return(
+        <div className="game-over">
+            <h2>Game Over!</h2>
+            <h4>Stats:</h4>
+            <p>You had {clickCount} clicks left</p>
+            <p>You clicked a total {totalClicks} times</p>
+            <ul>
+                {
+                    pumpkinType.map(function (p) {
+                    return (
+                    <li key={p.id}>
+                        {p.color} pumpkin clicks: {p.count}
+                    </li>
+                    )})
+                }
+            </ul>
+            
+        </div>
+        )
+    }
+    return(
         <div className="main">
-            <h2>Bring the Click Count to 0 before time runs out!</h2>
+        <h2>Bring the Click Count to 0 before time runs out!</h2>
             <h3> {timeLeft} seconds </h3>
             <button className= "timerButton" onClick={startTimer}>Start Timer</button>
             <button className= "timerButton" onClick={stopTimer}>Stop Timer</button>
             <button className= "timerButton" onClick={resetTimer}>Reset Timer</button>
             
-            <p>Current Clicks: {clickCount}</p>
-            <button className= "pumpkinButton" onClick={handleClick()}>
-                <img src={randImg().img} alt="pumpkin"/>
+            <p>Current Clicks Remaining: {clickCount}</p>
+            <p>Total Clicks: {totalClicks}</p>
+            <button className= "pumpkinButton" onClick={handleClick}>
+                <img 
+                    src={pumpkin.img} 
+                    alt="pumpkin"
+                    style={{
+                        width: "150px",
+                        height: "150px",
+                        borderRadius: "12px",
+                        padding: "5px",
+                    }}
+                />
             </button>
         </div>
-        )
-    }while(timer < 0 && clickCount > 0) 
-
-    return(
-        <div className="game-over">
-            <h2>Game Over!</h2>
-            <h4>Stats:</h4>
-            <p>You had {clickCount} clicks left</p>
-            <p>You clicked a total {clickCount} times</p>
-            <ul>
-                {pumpkinType.map(p => (
-                    <li>{p.color} pumpkin clicks: {p.count}</li>
-                ))}
-            </ul>
-            
-        </div>
     )
+    
 }
 
 
